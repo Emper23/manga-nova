5d26e489060076033381142dabd1e217c853e7e3
Emper23 <ysthrthxngkhngxwm@gmail.com>
2026-09-12 19:53:17 +0700

    Sync src/main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import "./styles/app.css";
+import "./styles/cards.css";
+import "./styles/detail.css";
+import "./styles/reader.css";
+import "./styles/pages.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
